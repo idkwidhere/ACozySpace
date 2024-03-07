d97d36d3d111693bc98c6fed49f75321a69daeeb
Big Johnson <73569000+idkwidhere@users.noreply.github.com>
2024-03-06 20:31:56 -0500

    Initial Commit
    Init

---
+++ scripts/player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+class_name Player
+
+const SPEED = 300.0
+var accel = 50
+@onready var player_sprite = $PlayerSprite
+
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_vector("a", "d", "w", "s")
+	
+	velocity.x = move_toward(velocity.x, SPEED * direction.x, accel)
+	velocity.y = move_toward(velocity.y, SPEED * direction.y, accel)
+	
+	if velocity.y > 0:
+		global_rotation_degrees = 180
+	if velocity.y < 0:
+		global_rotation_degrees = 0
+	if velocity.x > 0:
+		global_rotation_degrees = 90
+	if velocity.x < 0:
+		global_rotation_degrees = -90
+	print(velocity)
+
+	move_and_slide()
---
+++ scripts/terminal.gd
@@ -0,0 +1,20 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_interact_area_body_entered(body):
+	if body is Player:
+		$Interact.visible = true
+
+func _on_interact_area_body_exited(body):
+	if body is Player:
+		$Interact.visible = false
