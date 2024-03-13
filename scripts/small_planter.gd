87af9d871ded01b7869aa908070442a8255c748c
Big Johnson <73569000+idkwidhere@users.noreply.github.com>
2024-03-13 10:51:17 -0400

    update small_planter
    todo: add a way to plant seeds

--- scripts/small_planter.gd
+++ scripts/small_planter.gd
@@ -1,18 +1,22 @@
 extends StaticBody2D
 
 var seeds_planted = []
+@onready var seeds = $Seeds
 
-#plant import
-#var potato = preload("res://scenes/potato_plant.tscn")
+#plants
+const POTATO_PLANT = preload("res://scenes/plants/potato_plant.tscn")
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print(len(seeds.get_children()))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+		
 
 func add_seed(type, marker):
 	print("seed planted")
